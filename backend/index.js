f922441197f2d041e01116f5f3dee08f00a87b67
sachinnautiyal-github <nautiyalsachin217@gmail.com>
2024-02-26 07:52:54 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,11 @@ connectToMongo();
 
 const express = require('express')
 const app = express()
-app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 const port = process.env.PORT
 
 app.use(express.json());
